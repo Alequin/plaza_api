50b489c25e24ed1032ace1fc222dfc9b35c9bfbd
James Cox <j-cox03@hotmail.com>
2017-08-08 17:59:38 +0100

    Stock: made method to open and close database as needed

--- controllers/stock.js
+++ controllers/stock.js
@@ -3,51 +3,63 @@ var stockRouter = express.Router();
 var path = require("path");
 
 var mongoClient = require("mongodb").MongoClient;
-var db;
-var stock;
 
-mongoClient.connect("mongodb://localhost:27017/plaza",
-  function(err, database){
-    if(err){
-      console.log(err);
-      return;
+function connect(action){
+  mongoClient.connect("mongodb://localhost:27017/plaza",
+    function(err, database){
+      if(err){
+        console.log(err);
+        return;
+      }
+
+      console.log("Connected to database");
+      db = database;
+      action(db.collection("stock"));
+      db.close
     }
-
-    db = database;
-    stock = db.collection("stock");
-    console.log("Connected to database");
-  }
-);
+  );
+}
 
 stockRouter.get("/", function(req, res){
-  stock.find().toArray(function(err, results){
-    res.json({response: "stock home route", result: results});
+  connect(function(collection){
+    collection.find().toArray(function(err, results){
+      res.json({response: "stock home route", result: results});
+    });
   });
 });
 
 stockRouter.get("/name/:name", function(req, res){
-  stock.find({name: req.params.name}).toArray(function(err, results){
-    res.json({response: "stock single item by name route", result: results});
+  connect(function(collection){
+    collection.find({name: req.params.name}).toArray(function(err, results){
+      res.json({response: "stock single item by name route", result: results});
+    });
   });
 });
 
 stockRouter.get("/category/:category", function(req, res){
-  stock.find({category: req.params.category}).toArray(function(err, results){
-    res.json({response: "stock single item by category route", result: results});
+  connect(function(collection){
+    collection.find({category: req.params.category}).toArray(function(err, results){
+      res.json({response: "stock single item by category route", result: results});
+    });
   });
 });
 
 stockRouter.post("/", function(req, res){
-  stock.save(req.body, function(err, results){
-    res.json({response: "stock save route", result: results});
+  connect(function(collection){
+    collection.save(req.body, function(err, results){
+      res.json({response: "stock save route", result: results});
+    });
   });
 });
 
 
 stockRouter.delete("/", function(req, res){
-  stock.deleteMany(function(err, results){
-    res.json({response: "stock delete all route", result: results});
+  connect(function(collection){
+    collection.deleteMany(function(err, results){
+      res.json({response: "stock delete all route", result: results});
+    });
   });
+
 });
 
 module.exports = stockRouter;
